ff493d75637b14fd72334575d5c7b99f9a5f264c
sullivana10 <sullivana10@gmail.com>
2022-06-25 15:15:37 -0500

    footer

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Video1 from "./components/video1/Video1";
 import Video2 from "./components/video2/Video2";
 import Apparel from "./components/apparel/Apparel";
 import News from "./components/news/News";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
       <Apparel />
       <Video2 />
       <News />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import "./footer.scss";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="page-width">
+        <div className="content">
+          {/* <div className="blank"></div> */}
+          <div className="left">
+            <a href="/" className="search">
+              Search
+            </a>
+            <a href="/" className="contact">
+              Contact Us
+            </a>
+            <a href="/" className="returns">
+              Returns & Exchanges
+            </a>
+            <a href="/" className="privacy">
+              Privacy Policy
+            </a>
+            <a href="/" className="shipping">
+              Shipping FAQ
+            </a>
+            <a href="/" className="terms">
+              Site Use Terms & Conditions
+            </a>
+          </div>
+          <div className="right">
+            <p className="signUp">sign up and save</p>
+            <p className="subscribe">
+              Subscribe to get special offers, free giveaways, and
+              once-in-a-lifetime deals
+            </p>
+            <form action="/">
+              <input
+                type="email"
+                placeholder="Enter your email"
+                className="email"
+              />
+              {/* <button className="submit">Submit</button> */}
+              {/* <div className="mailIcon">
+                <MailOutlineIcon />
+              </div> */}
+            </form>
+            <div className="iconContainer">
+              <div className="icon">
+                <InstagramIcon
+                  onClick={(event) =>
+                    window.open(
+                      "https://www.instagram.com/bythethousands",
+                      "_blank"
+                    )
+                  }
+                  sx={{ fontSize: 30 }}
+                />
+              </div>
+              <div className="icon">
+                <FacebookIcon
+                  onClick={(event) =>
+                    window.open(
+                      "https://www.facebook.com/bythethousands",
+                      "_blank"
+                    )
+                  }
+                  sx={{ fontSize: 30 }}
+                />
+              </div>
+              <div className="icon">
+                <TwitterIcon
+                  onClick={(event) =>
+                    window.open(
+                      "https://www.twitter.com/bythethousands",
+                      "_blank"
+                    )
+                  }
+                  sx={{ fontSize: 30 }}
+                />
+              </div>
+            </div>
+          </div>
+          {/* <div className="blank"></div> */}
+        </div>
+      </div>
+    </div>
+  );
+}
